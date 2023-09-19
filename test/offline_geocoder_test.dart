3524139bf6bbced86a32f0b3fc21bcc14eee8070
Robbendebiene <robbendebiene@mailbox.org>
2023-09-19 16:06:24 +0200

    Add initial dart package files

---
+++ test/offline_geocoder_test.dart
@@ -0,0 +1,16 @@
+import 'package:offline_geocoder/offline_geocoder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}

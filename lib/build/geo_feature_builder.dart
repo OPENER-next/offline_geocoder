088e1ac4b6a5e071963a25b802bbb74b77bfc15f
Robbendebiene <robbendebiene@mailbox.org>
2023-10-25 09:23:42 +0200

    Update GeoCoder builder

--- lib/build/geo_feature_builder.dart
+++ lib/build/geo_feature_builder.dart
@@ -6,12 +6,18 @@ import 'package:code_builder/code_builder.dart';
 import 'package:dart_style/dart_style.dart';
 import 'package:recase/recase.dart';
 
+
+Builder geoFeatureBuilder(BuilderOptions options) => GeoFeatureBuilder(options);
+
+
 class GeoFeatureBuilder implements Builder{
 
   final BuilderOptions options;
 
   GeoFeatureBuilder(this.options);
-  
+
+  static const _src = 'package:offline_geocoder/offline_geocoder.dart';
+
   @override
   Map<String, List<String>> get buildExtensions => {
     options.config['source_file'] : [ options.config['output_file'] ],
@@ -21,38 +27,39 @@ class GeoFeatureBuilder implements Builder{
   Future<void> build(BuildStep buildStep) async {
     final AssetId inputId = buildStep.inputId;
     try {
-        final geoJSONData = await _readFile(File(inputId.path));
-        final propertiesToExtract = options.config['extract_properties'].map<String,String>(
-            (key, value) => MapEntry<String, String>(key.toString(), value.toString())
-        );
-        final code = generateFileCode(geoJSONData,propertiesToExtract);
-        final outputId = AssetId(inputId.package, inputId.path
-          .replaceFirst(options.config['source_file'],options.config['output_file'])
-        );
-        await buildStep.writeAsString(outputId, code);
+      final geoJSONData = await _readFile(File(inputId.path));
+      final propertiesToExtract = options.config['extract_properties'].map<String,String>(
+        (key, value) => MapEntry<String, String>(key.toString(), value.toString())
+      );
+
+      final library = Library((b) => b
+        ..body.add(buildGeoFeatureClass(geoJSONData, propertiesToExtract))
+        ..body.add(buildGeoCoderClass(geoJSONData, propertiesToExtract))
+      );
+      final code = DartFormatter().format(library.accept(
+        DartEmitter(allocator: Allocator()),
+      ).toString());
+
+      final outputId = AssetId(
+        inputId.package,
+        inputId.path.replaceFirst(
+          options.config['source_file'],options.config['output_file'],
+        ),
+      );
+      await buildStep.writeAsString(outputId, code);
     } catch (e) {
-        throw 'Failed to generate GeoCoder class: $e';
+      throw 'Failed to generate GeoCoder class: $e';
     }
   }
 
-  String generateFileCode(Map<String, dynamic> geoJSONData, Map<String, String> propertiesToExtract){
-    final library = Library((b) => b
-      ..directives.add(Directive.import('package:offline_geocoder/offline_geocoder.dart'))
-      ..directives.add(Directive.import('package:latlong2/latlong.dart'))
-      ..body.add(buildGeoFeatureClass(geoJSONData, propertiesToExtract))
-      ..body.add(buildGeoCoderClass(geoJSONData, propertiesToExtract))
-    );
-    final dartfmt = DartFormatter();
-    return dartfmt.format('${library.accept(DartEmitter.scoped())}').toString();
-  }
+
 
   Future<Map<String, dynamic>> _readFile(File inputFile) async {
     if (!(await inputFile.exists())) {
       throw FileSystemException('The file ${inputFile.path} does not exist');
     }
-    String data;
     try {
-      data = await inputFile.readAsString();
+      final data = await inputFile.readAsString();
       return json.decode(data) as Map<String, dynamic>;
     } catch (e) {
       throw FileSystemException('The file $e can not be read');
@@ -60,19 +67,16 @@ class GeoFeatureBuilder implements Builder{
   }
 
   LiteralListExpression buildGeoFeatureListInstance(Map<String, dynamic> geoJSONData, Map<String, String> propertiesToExtract) {
-    final features = geoJSONData['features'].cast<Map<String, dynamic>>(); 
-    final geoFeatureRefer = refer('GeoFeature');
+    final features = geoJSONData['features'].cast<Map<String, dynamic>>();
+    final geoFeatureRefer = refer('GeoFeature', _src);
     final List<Expression> geoFeatureList = [];
+
     for (final featureData in features) {
       final List<Expression> geoFeatureParameters = [];
       if (featureData.containsKey('properties')) {
         final featureProperties = featureData['properties'];
         for (final entry in propertiesToExtract.entries) {
-          geoFeatureParameters.add(refer(
-            entry.value == 'String'
-              ? '"${featureProperties[entry.key]}"'
-            : '${featureProperties[entry.key]}',
-          ));
+          geoFeatureParameters.add(literal(featureProperties[entry.key]));
         }
       }
       final geometry = featureData['geometry'];
@@ -83,7 +87,7 @@ class GeoFeatureBuilder implements Builder{
           final coordinates = coordinatesGeoJSON.map<Iterable<Iterable<List<num>>>>(
             (e) => (e as Iterable).map<Iterable<List<num>>>(
               (e) => (e as Iterable).map<List<num>>((e) => e.cast<num>()),
-            )
+            ),
           );
           geoFeatureParameters.add(buildMultiPolygonInstance(coordinates));
         case 'Polygon':
@@ -95,66 +99,77 @@ class GeoFeatureBuilder implements Builder{
           throw 'The geometry type: $geometryType is not supported';
       }
       geoFeatureList.add(geoFeatureRefer.newInstance(geoFeatureParameters));
-    } 
+    }
     return literalList(geoFeatureList);
   }
 
+  /// Builds `MultiPolygon([Polygon(...), Polygon(...), ...])`
+
   Expression buildMultiPolygonInstance(Iterable<Iterable<Iterable<List<num>>>> coordinates) {
-    final multiPolygonRefer = refer('MultiPolygon');
+    final multiPolygonRefer = refer('MultiPolygon', _src);
     final polygons = coordinates.map(buildPolygonInstance);
     return multiPolygonRefer.newInstance([literalList(polygons)]);
   }
 
+  /// Builds `Polygon(Ring(...), [Ring(...), ...])`
+
   Expression buildPolygonInstance(Iterable<Iterable<List<num>>> coordinates) {
     final List<Expression> inner = [];
-    final polygonRefer = refer('Polygon');
+    final polygonRefer = refer('Polygon', _src);
     final iterator = coordinates.iterator;
+    // outer ring
     iterator.moveNext();
     final outer = buildRingInstance(iterator.current);
-    while(iterator.moveNext()) {
+    // inner rings
+    while (iterator.moveNext()) {
       inner.add(buildRingInstance(iterator.current));
-    } 
-    if(inner.isEmpty){
+    }
+    if (inner.isEmpty){
       return polygonRefer.newInstance([outer]);
     }
-    else{
+    else {
       return polygonRefer.newInstance([outer, literalList(inner)]);
     }
   }
 
+  /// Builds `Ring([LatLng(...), ...])`
+
   Expression buildRingInstance(Iterable<List<num>> coordinates) {
-    final ringRefer = refer('Ring');
+    final ringRefer = refer('Ring', _src);
     final listPoints = coordinates.map(
       (geoPoint) => buildLatLngInstance(geoPoint[1], geoPoint[0]),
     );
     return ringRefer.newInstance([literalList(listPoints)]);
   }
 
+  /// Builds `LatLng(...)`
+
   Expression buildLatLngInstance(num latitude, num longitude) {
-    final latLngRef = refer('LatLng');
+    final latLngRef = refer('LatLng', 'package:latlong2/latlong.dart');
     return latLngRef.newInstance([literalNum(latitude),literalNum(longitude)]);
   }
 
+  /// Builds `class GeoFeature extends GeoFeatureBase { ... }`
+
   Class buildGeoFeatureClass(Map<String, dynamic> geoJSONData, Map<String, String> propertiesToExtract) {
-    final List<Field> classFields = [];
-    final List<Parameter> constructorParameters = [];
-    propertiesToExtract.forEach((key, value) { 
-      classFields.add(Field((b) => b
+    final classFields = propertiesToExtract.entries.map((entry) {
+      return Field((b) => b
         ..modifier = FieldModifier.final$
-        ..name = ReCase(key).camelCase 
-        ..type = Reference(value,'dart:core').type
-        )
-      );
-      constructorParameters.add(Parameter((b) => b
-        ..name = ReCase(key).camelCase
-        ..named = true
-        ..toThis = true
-        )
+        ..name = ReCase(entry.key).camelCase
+        ..type = Reference(entry.value,'dart:core').type
       );
     });
+    final constructorParameters = propertiesToExtract.keys.map((name) {
+      return Parameter((b) => b
+        ..name = ReCase(name).camelCase
+        ..named = true
+        ..toThis = true
+      );
+    });
+
     final geoFeatureClass = ClassBuilder()
       ..name = 'GeoFeature'
-      ..extend = refer('GeoFeatureBase')
+      ..extend = refer('GeoFeatureBase', _src)
       ..fields.addAll(classFields)
       ..constructors.add(Constructor((b) => b
         ..constant = true
@@ -163,15 +178,18 @@ class GeoFeatureBuilder implements Builder{
           ..name = 'area'
           ..named = true
           ..toSuper = true
-        ))
+        )),
       ));
     return geoFeatureClass.build();
   }
+
+  /// Builds `class GeoCoder { ... }`
 
   Class buildGeoCoderClass(Map<String, dynamic> geoJSONData, Map<String, String> propertiesToExtract) {
     final geoFeatureList = buildGeoFeatureListInstance(geoJSONData, propertiesToExtract);
     final geoCoderClass = ClassBuilder()
       ..name = 'GeoCoder'
+      ..abstract = true
       ..fields.add(Field((b) => b
         ..name = 'geoFeatures'
         ..modifier = FieldModifier.constant
@@ -180,16 +198,15 @@ class GeoFeatureBuilder implements Builder{
       ))
       ..methods.add(Method((b) => b
         ..name = 'getFromLocation'
-        ..static = true 
+        ..static = true
         ..returns = refer('GeoFeature?')
         ..requiredParameters.add(Parameter((b) => b
           ..name = 'point'
-          ..type = refer('LatLng')
+          ..type = refer('LatLng', 'package:latlong2/latlong.dart')
         ))
-        ..body = refer('firstGeoFeatureContainingPoint<GeoFeature>')
+        ..body = refer('firstGeoFeatureContainingPoint<GeoFeature>', _src)
           .call([refer('point'), refer('geoFeatures')]).code
-      ))
-    ;
+      ));
     return geoCoderClass.build();
   }
 }
--- lib/offline_geocoder.dart
+++ lib/offline_geocoder.dart
@@ -1,8 +1,5 @@
 library offline_geocoder;
+
 export '/src/geo_feature_base.dart';
 export '/src/area.dart';
-
-import 'package:build/build.dart';
-import 'build/geo_feature_builder.dart';
-
-Builder geoFeatureBuilder(BuilderOptions options) => GeoFeatureBuilder(options);
+export '/build/geo_feature_builder.dart';

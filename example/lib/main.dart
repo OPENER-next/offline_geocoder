84e10debd298a98506277eb3b941641b7d22a945
Robbendebiene <robbendebiene@mailbox.org>
2023-10-25 09:25:03 +0200

    Update geocoder example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,12 @@
-// ignore_for_file: avoid_print
-import 'package:example/offline_geocoder/offline_geocoder.dart';
 import 'package:latlong2/latlong.dart';
+
+// run `dart run build_runner build` or `dart run build_runner watch` to generate the import file
+import 'offline_geocoder/offline_geocoder.dart';
 
 
 void main() {
   final result = GeoCoder.getFromLocation(const LatLng(-18.1248, 178.4501));
-  print(result?.sovereignt);
+  print(result?.name);
   print(result?.isoA2);
+  print(result?.level);
 }
-
